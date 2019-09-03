0bb3aca5539db2b7aca35ba44a485aadab17a8df
retyidoro <retyidoro@yahoo.com>
2019-09-03 13:24:35 +0300

    Update week4.R
    in progress the 4th

--- week4.R
+++ week4.R
@@ -7,37 +7,154 @@ names(outcome)
 outcome[, 11] <- as.numeric(outcome[, 11]) #use as numeric values
 hist(outcome[, 11])
 
-#2nd assignment
-states <- unique(outcome[, 7])
-print(states[2])
-library(data.table)
+#2nd assignment: given state, given outcome> the best hospital selection
+library(readr)
+library(dplyr)
+
+outcomes <- read_csv("outcome-of-care-measures.csv")
+states <- sort(unique(outcome$State))
+print(states)
 
 best <- function(state, outcome) {
-  #read the data
-  data1 <- fread("outcome-of-care-measures.csv", sep=",")
-  data <- as.data.table(data1)
+  #read the data done outside of the function
   
-  #checking parameters
-  if (is.element(state, states) && is.element(outcome, c("heart attack", "heart failure", "pneumonia"))) {
-    
-    #getting the correct index
-    switch(outcome,
-           "heart attack" = {
-             idx <- 11
-           },
-           "heart failure" = {
-             idx <- 17
-           },
-           "pneumonia" = {
-             idx <- 23
-           }
-           )
-    
-    hospitals_in_state <- data[state == "TX"] #getting every matching rows
-    ordered_hospitals <- hospitals_in_state[ order( hospitals_in_state[, idx], hospitals_in_state[, 2]), ]
-    
-    print(nrow(hospitals_in_state))
+  #check the validity of parameters
+  if(!state %in% states) stop("Not a valid state!")
+  if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("Not a valid outcome")
+  
+  idx <- 11
+  #getting the index of needed column
+  if(outcome == "heart failure") {
+    idx <- 17
   }
+  else {
+    if (outcome == "pneumonia") {
+      idx <- 23
+    }
+  }
+  print(idx)
+  #subsetting the dataframe
+  df_small <- outcomes %>% select(c(2, 7, idx)) #state and illness
+  colnames(df_small) <- c("Hospital", "State", "Outcome")
+  
+  df_state <- df_small %>% filter(State == state) #the right state
+  #print(df_state)
+  df_arr <- df_state %>% arrange(as.double(Outcome), Hospital)
+  #print(df_arr)
+
+  df_arr[1, ]
 }
 
-best("TX", "heart attack")
+res <- best("NY", "hert attack")
+print(res)
+
+
+#3 ranking hospitals by outcome in a state
+rankhospital <- function(state, outcome, num = "best") {
+  #already red data
+  
+  #check the validity of parameters
+  if(!state %in% states) stop("Not a valid state!")
+  if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("Not a valid outcome")
+  
+  idx <- 11
+  #getting the index of needed column
+  if(outcome == "heart failure") {
+    idx <- 17
+  }
+  else {
+    if (outcome == "pneumonia") {
+      idx <- 23
+    }
+  }
+  print(idx)
+  
+  #subsetting the dataframe
+  df_small <- outcomes %>% select(c(2, 7, idx)) #state and illness
+  colnames(df_small) <- c("Hospital", "State", "Outcome")
+  
+  df_state <- df_small %>% filter(State == state) #the right state
+  
+  
+  #print(df_state)
+  df_state$Outcome <- as.numeric(as.character(df_state$Outcome)) #replace not available with na
+  
+  df_arr <- df_state %>% arrange(as.double(Outcome), Hospital) #order by col
+  
+  df_av <- df_arr %>% filter(Outcome != "NA")
+  print(df_av)
+  
+  #print(df_arr)
+  print("az utolso")
+  print(df_av[nrow(df_av), ])
+  
+  #check the rank
+  if (num == "best") num <- 1
+  if (num == "worst") num <- nrow(df_av)
+  
+  print(df_av[num, ])
+}
+
+res <- rankhospital("NM", "heart attack", 5000)
+print(res)
+
+
+
+
+#4   ranking hospitals in all states
+
+rankall <- function(outcome, num = "best") {
+  #data red
+  
+  if (!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("Not a valid outcome")
+  
+  idx <- 11
+  #getting the index of needed column
+  if(outcome == "heart failure") {
+    idx <- 17
+  }
+  else {
+    if (outcome == "pneumonia") {
+      idx <- 23
+    }
+  }
+  print(idx)
+  print(outcome)
+  
+  #get the tree columns
+  df_small <- outcomes %>% select(c(2, 7, idx))
+  colnames(df_small) <- c("Hospital", "State", "Outcome")
+  df_small$Outcome <- as.numeric(as.character(df_small$Outcome)) #replace not available with na
+  df_small <- df_small %>% filter(Outcome != "NA")
+  
+  hospitals <- c()
+  
+  for (state in states) {
+    
+    df_state <- df_small %>% filter(State == state)
+    df_arr <- df_state %>% arrange(as.double(Outcome), Hospital) #order by col
+    print("a df hossza")
+    print(nrow(df_arr))
+    #check the rank
+    if (num == "best") { num <- 1 }
+    if (num == "worst") { num <- nrow(df_arr) }
+    print("a rank szama: ")
+    print(num)
+    print(df_arr[num, ])
+    hospitals <- c(hospitals, df_arr[num, ]$Hospital)
+    
+  } 
+  #print(hospitals)
+  df <- data.frame(states, hospitals)
+}
+
+res <- rankall("heart failure", 10)
+print(res)
+
+
+
+
+
+
+
+
